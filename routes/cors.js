b14651df46c94c0f4287ef55851875aeb92dbb94
moazkhan05 <48621157+moazkhan05@users.noreply.github.com>
2020-09-20 04:25:45 +0500

    CORS

---
+++ routes/cors.js
@@ -0,0 +1,27 @@
+/* setting cors file cross-origin resource sharing 
+    this file consist of configuration of cors by using
+      npm install cors@2.8.4 --save
+    
+      Configure your Express application to 
+      use the cors module to support CORS on various endpoints
+*/
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const whitelist = ['http://localhost:3000', 'https://localhost:3443'];
+var corsOptionsDelegate = (req, callback) => {
+    var corsOptions;
+    console.log(req.header('Origin'));
+    if(whitelist.indexOf(req.header('Origin')) !== -1) {
+        corsOptions = { origin: true };
+    }
+    else {
+        corsOptions = { origin: false };
+    }
+    callback(null, corsOptions);
+};
+
+exports.cors = cors();
+exports.corsWithOptions = cors(corsOptionsDelegate);
