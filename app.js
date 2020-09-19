ac29c1a6850b9e50ea96da9ece49155094305230
moazkhan05 <48621157+moazkhan05@users.noreply.github.com>
2020-09-20 02:35:42 +0500

    file uploads

--- app.js
+++ app.js
@@ -9,11 +9,15 @@ var FileStore = require('session-file-store')(session);
 var passport = require('passport');
 var authenticate = require('./authenticate');
 
+//-----------------------Routers
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
+//-----------------------END Routers
+
 
 var config = require('./config'); //secret key and url
 
@@ -66,6 +70,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
